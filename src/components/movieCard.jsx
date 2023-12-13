bc8a31c6587f748e8eccbff7a504cf2165b8090b
jadsoncerqueira <jadsoncerqueira97@gmail.com>
2023-12-13 17:48:52 -0300

    Arrumando tamanho dos cards de filmes

--- src/components/movieCard.jsx
+++ src/components/movieCard.jsx
@@ -8,42 +8,37 @@ import { useContext } from "react";
 
 const apiImg = import.meta.env.VITE_IMAGE;
 
-export default function MovieCard(movie, isLoading) {
+export default function MovieCard(movie) {
   const navigate = useNavigate();
 
   const { setQuery } = useContext(querySearchContext);
 
   const { poster_path, vote_average, id } = movie.movie;
   return (
-    <>
-      <div className="card-div">
-        <button
-          onClick={() => {
-            setQuery("");
-            navigate(`/movie/detalhes/${id}`);
+    <div className="card-div">
+      <button
+        onClick={() => {
+          setQuery("");
+          navigate(`/movie/detalhes/${id}`);
+        }}
+        className="btn-movie"
+      >
+        <div className="pont">
+          {" "}
+          <StarIcon sx={{ color: "#ffb700", width: "17px" }} />{" "}
+          {vote_average.toFixed(1)}
+        </div>
+        <img
+          className="img-movie"
+          data-src={`${apiImg}w185${poster_path}`}
+          src={placeholderImage}
+          onLoad={({ target }) => {
+            const dataImage = target.getAttribute("data-src");
+            target.setAttribute("src", dataImage);
           }}
-          className="btn-movie"
-        >
-          {isLoading.isLoading && (
-            <div className="pont">
-              {" "}
-              <StarIcon sx={{ color: "#ffb700", width: "17px" }} />{" "}
-              {vote_average.toFixed(1)}
-            </div>
-          )}
-          <img
-            className="img-movie"
-            data-src={`${apiImg}w185${poster_path}`}
-            src={placeholderImage}
-            onLoad={({ target }) => {
-              const dataImage = target.getAttribute("data-src");
-              target.setAttribute("src", dataImage);
-            }}
-            alt=""
-          />
-        </button>
-        {/* <p>{title}</p> */}
-      </div>
-    </>
+          alt=""
+        />
+      </button>
+    </div>
   );
 }
